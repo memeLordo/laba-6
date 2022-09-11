1dde7823b6ecc853f8563adeefdec3440ca45e39
Sima <100208922+memeLordo@users.noreply.github.com>
2022-09-11 16:20:02 +0300

    update: MaxElement by person

--- Server/src/main/java/SetOfCommands/MaxElement.java
+++ Server/src/main/java/SetOfCommands/MaxElement.java
@@ -20,29 +20,15 @@ public class MaxElement extends Command {
     @Override
     public void go() {
 
-        if (inputCommand.equals(getColumns()[0])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getId)).get().getId());
-        if (inputCommand.equals(getColumns()[1])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getName)).get().getName());
-        if (inputCommand.equals(getColumns()[2])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getCoordinates)).get().getCoordinates());
-        if (inputCommand.equals(getColumns()[3])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getCreationDate)).get().getCreationDate());
-        if (inputCommand.equals(getColumns()[4])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getHeight)).get().getHeight());
-        if (inputCommand.equals(getColumns()[5])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getBirthday)).get().getBirthday());
-        if (inputCommand.equals(getColumns()[6])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getWeight)).get().getWeight());
-        if (inputCommand.equals(getColumns()[7])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getEyeColor)).get().getEyeColor());
-        if (inputCommand.equals(getColumns()[8])) System.out.println(getPeople_data().stream().max(Comparator.comparing(Person::getLocation)).get().getLocation());
+        if (inputCommand.equals(getColumns()[0])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getId)).get());
+        if (inputCommand.equals(getColumns()[1])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getName)).get());
+        if (inputCommand.equals(getColumns()[2])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getCoordinates)).get());
+        if (inputCommand.equals(getColumns()[3])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getCreationDate)).get());
+        if (inputCommand.equals(getColumns()[4])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getHeight)).get());
+        if (inputCommand.equals(getColumns()[5])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getBirthday)).get());
+        if (inputCommand.equals(getColumns()[6])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getWeight)).get());
+        if (inputCommand.equals(getColumns()[7])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getEyeColor)).get());
+        if (inputCommand.equals(getColumns()[8])) Show.showPerson(getPeople_data().stream().max(Comparator.comparing(Person::getLocation)).get());
 
-
-
-
-//        for (Person chel : getPeople_data()) {
-//            maxId = chel.getId() > maxId.get() ? chel.getId() : maxId;
-//            maxHeight = chel.getHeight() > maxHeight ? chel.getHeight() : maxHeight;
-//            maxWeight = chel.getWeight() > maxWeight ? chel.getWeight() : maxWeight;
-//            maxBirth = chel.getBirthday().isAfter(maxBirth) ? chel.getBirthday() : maxBirth;
-//        }
-//        if (inputCommand.equals(getColumns()[0])) this.findIn(maxId);
-//        else if (inputCommand.equals(getColumns()[4])) this.findIn(maxHeight);
-//        else if (inputCommand.equals(getColumns()[5])) this.findIn(maxWeight);
-//        else if (inputCommand.equals(getColumns()[6])) this.findIn(maxBirth);
-//        else System.out.println("Такого элемента нет. Введите 'info' для справки.");
     }
 }
