120306a61323a777a45dd2e73685d8afe50a9b17
Sima <100208922+memeLordo@users.noreply.github.com>
2022-10-26 16:39:32 +0300

    update: Generic Message

--- Charon/src/main/java/ServerOperation/Message.java
+++ Charon/src/main/java/ServerOperation/Message.java
@@ -1,11 +1,52 @@
 package ServerOperation;
 
-import Parameters.Person;
+import java.io.Serializable;
 
-public class ServerRequestClass {
-    String command;
-    Person PersonData;
-    public ServerRequestClass(String command, Person person) {
-        super(command,person);
+public class Message<T extends Serializable> implements Serializable {
+    private String command;
+    private T argument;
+
+    public Message(String command, T argument) {
+        this.command = command;
+        this.argument = argument;
     }
+
+    public T getArgument() {
+        return argument;
+    }
+
+    public void setArgument(T argument) {
+        this.argument = argument;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+    public static Message<String> mapFind(String textLine) {
+        String inputCommand = "";
+        String[] setOfCommands = textLine.split(" ");
+        try {
+
+            inputCommand=setOfCommands[1];
+
+        } catch (NullPointerException e1) {
+            e1.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException ignored) {}
+        finally {
+            return new Message<>(setOfCommands[0],inputCommand);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  "command='" + command + '\'' +
+                ", argument='" + argument + "'";
+    }
+    //    public static void clearInputCommand() {
+//        inputCommand = "";
+//    }
 }
